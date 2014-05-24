9f176bffb9b2a2c23c11a615df19a243b5d03d43
K <celfrost@hotmail.com>
2014-05-24 12:56:06 -0400

    init index page

---
+++ server.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+
+team_and_scores = [
+  {
+    home_team: "Patriots",
+    away_team: "Broncos",
+    home_score: 7,
+    away_score: 3
+  },
+  {
+    home_team: "Broncos",
+    away_team: "Colts",
+    home_score: 3,
+    away_score: 0
+  },
+  {
+    home_team: "Patriots",
+    away_team: "Colts",
+    home_score: 11,
+    away_score: 7
+  },
+  {
+    home_team: "Steelers",
+    away_team: "Patriots",
+    home_score: 7,
+    away_score: 21
+  }
+]
+
+get '/' do
+	@team_and_scores = team_and_scores
+	erb :index
+end
+
+get '/leaderboard' do
+	erb :leaderboard
+end
